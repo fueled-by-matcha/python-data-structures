ae41874c3fe07e4479621d70a643f9e29471058a
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2023-09-15 20:00:46 -0400

    Create searchcademy.py

---
+++ searchcademy.py
@@ -0,0 +1,7 @@
+def sparse_search(data, search_val):
+  first = 0
+  last = len(data) - 1
+  while first < last:
+    mid = (first + last) // 2
+  print("Data: " + str(data))
+  print("Search Value: " + str(search_val))
