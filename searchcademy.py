fe7341518eb5cdade313aff86264f00f6a049287
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2023-09-18 19:45:12 -0400

    Update searchcademy.py
    added functionality for pointers

--- searchcademy.py
+++ searchcademy.py
@@ -1,7 +1,31 @@
 def sparse_search(data, search_val):
   first = 0
-  last = len(data) - 1
+  last = len(data) - 1 
   while first < last:
     mid = (first + last) // 2
-  print("Data: " + str(data))
-  print("Search Value: " + str(search_val))
+    if mid == None:
+      left = mid - 1
+      right = mid + 1
+      if search_val < data[mid]:
+        last = mid - 1
+      if search_val > data[mid]:
+        first = mid + 1
+      while True:
+        if left < first and right > last:
+          print(f'{search_val} is not in the data set')
+          return None
+        elif right <= last and data[right] != None:
+          mid = right
+          break
+        elif left >= first and data[left] != None:
+          mid = left
+          break
+        right += 1
+        left -= 1
+      if data[mid] == search_val:
+        print(f'{search_val} found at position {mid}')
+        return
+      print(f'{search_val} is not in the dataset')
+
+print("Data: " + str(data))
+print("Search Value: " + str(search_val))
