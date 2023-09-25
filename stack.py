6724a127d96749d1a2c50b8f6a26d7ec4939b44e
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2023-09-25 19:23:52 -0400

    Update stack.py
    added helper methods has_space() and is_empty()

--- stack.py
+++ stack.py
@@ -8,21 +8,33 @@ class Stack:
     self.size = 0
 
   def peek(self):
-    if self.size > 0:
+    if not self.is_empty():
       return self.top_value.get_value()
     else:
       print("This stack is empty!")
 
   def push(self, value):
-    item = Node(value)
-    item.set_next_node(self.top_value)
-    self.top_value = item
+    if self.has_space() == True:
+      item = Node(value)
+      item.set_next_node(self.top_value)
+      self.top_value = item
+      self.size += 1
+    else:
+      print("This stack is full!")
 
   def pop(self):
-    if self.size > 0:
+    if not self.is_empty():
       item_to_remove = self.top_item
       self.top_item = item_to_remove.get_next_node()
       self.size -= 1
       return item_to_remove.get_value()
     else:
       print("This stack is empty!")
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self):
+    return self.size == 0
+
+  
